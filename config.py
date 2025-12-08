9e0c904a543023932d883c367ed58d852a51b8f8
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-12-09 02:17:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/heavenwrites90")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Out_Update_Bot")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Queenworld142")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
